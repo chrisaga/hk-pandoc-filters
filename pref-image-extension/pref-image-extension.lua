c5b33c50db28f414adfa1aabf73ae3c8beb9cc94
chrisaga <christophe.agathon@gmail.com>
2022-06-06 14:31:30 +0200

    Add filter pref-image-extension.lua

---
+++ pref-image-extension/pref-image-extension.lua
@@ -0,0 +1,57 @@
+--[[
+pref-image-extension
+            When image exists in different format (identified by file extension)
+	    use the prefered one depending on the output document format.
+	    File extension of the included image can be empty.
+	    If not, and a prefered format exists for the image, it will be used.
+
+Copyright:  © 2021 Christophe Agathon <christophe.agathon@gmail.com>
+License:    MIT – see LICENSE file for details
+
+Output:	    pdf, html
+
+TODO:	   - latex output : force extension to be empty since latex has it's own
+	     prefered image format mechanysm.
+	   - get list of prefered extension in metadata to override hardcoded
+	     defaults.
+
+Limitations :
+           - don't know how to get resource-path, if defined
+		(not in environment; in some metadata?)
+	   - no way to prevent extension replacement for a particular image if a
+	     prefered one exists.
+--]]
+-- Module pandoc.path is required and was added in version 2.12
+PANDOC_VERSION:must_be_at_least '2.12'
+
+local List = require 'pandoc.List'
+local path = require 'pandoc.path'
+--local system = require 'pandoc.system'
+
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
+function Image(image)
+  local path, ext = path.split_extension(image.src)
+  --[[
+  -- if extension exists do nothing
+  if ext ~= '' then return nil end
+  --]]
+
+  local pref_ext
+  if FORMAT:match 'latex' then
+    pref_ext = { 'pdf'; 'png'; 'jpg'; 'jpeg' }
+  elseif FORMAT:match 'html' then
+    pref_ext = { 'svg'; 'png'; 'jpg'; 'jpeg' }
+  end
+
+  -- chose 'best' extension
+  for _,ext in pairs(pref_ext) do
+    if file_exists(path .. '.' .. ext) then
+      image.src = path .. '.' .. ext
+      return image
+    end
+  end
+end
